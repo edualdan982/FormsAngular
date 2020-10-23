6ece66c0f0dba87c840c3dcd34e41bcdf3284436
edsarmiento1 <edsarmiento1@umsa.bo>
2020-10-23 14:51:11 -0400

    formulario reactivo con bulma

--- src/app/components/valCustomizado/form/form.component.ts
+++ src/app/components/valCustomizado/form/form.component.ts
@@ -1,42 +1,36 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { debounceTime } from 'rxjs/operators'
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.less']
+  styleUrls: ['./form.component.less'],
 })
 export class FormComponent implements OnInit {
-
   form: FormGroup;
 
-  constructor(
-    private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder) {
     this.buildForm();
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  private buildForm() {
+  private buildForm(): void {
     this.form = this.formBuilder.group({
       name: ['', [Validators.required]],
       date: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.email]],
-      text: ['', [Validators.required, Validators.maxLength(200)]],
+      text: ['', [Validators.required, Validators.maxLength(80)]],
       category: ['', [Validators.required]],
       gender: ['', [Validators.required]],
     });
 
-    this.form.valueChanges
-      .pipe(
-        debounceTime(500)
-      )
-      .subscribe(value => {
-        console.log(value);
-      });
+    this.form.valueChanges.pipe(debounceTime(500)).subscribe((value) => {
+      console.log(value);
+    });
   }
-  save(event: Event) {
+  save(event: Event): void {
     event.preventDefault();
     if (this.form.valid) {
       const value = this.form.value;
@@ -45,10 +39,34 @@ export class FormComponent implements OnInit {
       this.form.markAllAsTouched();
     }
   }
-  doSomething() {
+  doSomething(): void {
     console.log('click');
   }
-  get emailField(){
+  get emailField(): AbstractControl {
     return this.form.get('email');
+  }
+  get emailFieldIsValid(): boolean {
+    return this.emailField.touched && this.emailField.valid;
+  }
+  get emailFieldIsInvalid(): boolean {
+    return this.emailField.touched && this.emailField.invalid;
+  }
+  get nameField(): AbstractControl {
+    return this.form.get('name');
+  }
+  get nameFieldIsValid(): boolean {
+    return this.nameField.touched && this.nameField.valid;
+  }
+  get nameFieldIsInvalid(): boolean {
+    return this.nameField.touched && this.nameField.invalid;
+  }
+  get dateField(): AbstractControl {
+    return this.form.get('date');
+  }
+  get dateFielIsValid(): boolean {
+    return this.dateField.touched && this.dateField.valid;
+  }
+  get dateFielIsInvalid(): boolean {
+    return this.dateField.touched && this.dateField.invalid;
   }
 }
